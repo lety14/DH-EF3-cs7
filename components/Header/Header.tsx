e2463264991c4b6091c3757f51e395dfcc3725b0
Leti <leticiapistarelli@gmail.com>
2022-08-20 17:03:13 -0300

    add login page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,9 +6,20 @@ import styles from "../../styles/Header.module.css";
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <Image src="/red.png" layout="fixed" width={50} height={50} alt="logo" />
-      <Link href="/">Home</Link>
-      <Link href="/faqs">Preguntas Frecuentes</Link>
+      <div className={styles.headerContainer}>
+        <Image
+          src="/red.png"
+          layout="fixed"
+          width={50}
+          height={50}
+          alt="logo"
+        />
+        <Link href="/">Home</Link>
+        <Link href="/faqs">Preguntas Frecuentes</Link>
+      </div>
+      <button className={styles.loginButton}>
+        <Link href="/login">Login</Link>
+      </button>
     </header>
   );
 };
---
+++ pages/login.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import styles from "../styles/Login.module.css";
+import { useRouter } from "next/router";
+import useAuth from "../auth/auth.context";
+
+const Login: NextPage = () => {
+  const router = useRouter();
+  const { authenticated, login } = useAuth();
+
+  useEffect(() => {
+    if (authenticated) {
+      router.push("/");
+    }
+  }, [authenticated]);
+
+  const submitUser = async () => {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+    });
+    const data = await response.json();
+    const token = data.token;
+    login(token);
+  };
+
+  return (
+    <main className={styles.container}>
+      <Head>
+        <title>RandomIn - Iniciar Sesión</title>
+        <meta name="description" content="Iniciar Sesión" />
+      </Head>
+      <div className={styles.loginContainer}>
+        <div className={styles.loginForm}>
+          <h2 className={styles.colorText}>Iniciar Sesión</h2>
+          <div className={styles.input}>
+            <label>Usuario:</label>
+            <input type="text" />
+          </div>
+          <div className={styles.input}>
+            <label>Password:</label>
+            <input type="password" />
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.button} onClick={submitUser}>
+              Ingresar
+            </button>
+          </div>
+        </div>
+        <div className={styles.image}></div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
